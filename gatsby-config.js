a024753734d1717d2190db141a98e9768a044e0f
kryderius <krydegamer@gmail.com>
2020-11-20 16:43:05 +0100

    Dodano Sitemap i Robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `SpeedCopy Wynajem kserokopiarek`,
+    description: `Wynajem kserokopiarek Piotrków Łódź. Serwis i naprawy w cenie.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://speedcopy.pl`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,6 +29,8 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
